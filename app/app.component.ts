e857ec3380e87f90fb170bb2b1233e881caf4eeb
shivamd20 <shivamd20@gmail.com>
2019-04-14 02:59:13 +0530

    1 file modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,8 @@ import { Store, Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { Add, CountState, ClearState } from './app.state';
 
+//Click on click me button and you will see a ramdom number is printed and if you click on clearState button you will see that the number has gone from the UI. Now if you click on click me you will see that state is getting updated but UI is not getting updated
+
 @Component({
   selector: 'my-app',
   template: `
@@ -14,12 +16,16 @@ import { Add, CountState, ClearState } from './app.state';
 export class AppComponent  {
 
   @Select(state =>{
-    
+    try {
     console.log(state)
 
     return state.count.number.value
+    }catch (err){
+      console.log(err)
+      throw err;
+    }
   
-  }) count$: Observable<number>;
+  }) count$: Observable<number>;  // here the selector updates only till it gets state.count not undefined 
 
   constructor(private store: Store) {}
 
